9da55e11f951594c521300c388a10c8ad5826bcd
Aymeric GRANIER <shopidev34@gmail.com>
2025-01-04 16:42:22 +0100

    ajout du robot et du sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  // your config here
+  site: 'https://spotiguess.com', // Remplacez par l'URL de votre site
+  integrations: [sitemap()],
 });
